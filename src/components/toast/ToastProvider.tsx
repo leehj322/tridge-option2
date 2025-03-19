914db9abe7e4f117435dc555715261bb47a7427f
leehj322 <gudwns8956@naver.com>
2025-03-19 17:29:53 +0900

    feat: Add support for null duration
    - If duration is null, the toast will not auto-close

--- src/components/toast/ToastProvider.tsx
+++ src/components/toast/ToastProvider.tsx
@@ -9,18 +9,22 @@ export default function ToastProvider({ children }: { children: ReactNode }) {
    * 토스트를 추가하는 함수입니다.
    *
    * @param {string} message - 토스트에 보여질 메세지입니다.
-   * @param {number} [duration=3000] - 토스트가 auto close 동작을 하는 duration입니다.
+   * @param {number | null} [duration=3000] - 토스트가 auto close 동작을 하는 duration입니다.
    * @param {"top-right" | "top-left" | "top-center" | "bottom-right" | "bottom-left" | "bottom-center"} [position="bottom-right"] - Toast가 생성되는 위치입니다. 총 6가지입니다.
    */
   const showToastMessage: (
     message: string,
     position?: Toast["position"],
-    duration?: number,
+    duration?: number | null,
   ) => void = useCallback(
     (message, position = "bottom-right", duration = 3000) => {
       const id = Date.now(); // 과제 제한으로 라이브러리 없이 ID 생성 (uuid)
       setToasts((prev) => [...prev, { id, message, duration, position }]);
 
+      // duration이 null인 경우 toast 제거 동작 하지 않고 early return
+      if (duration === null) return;
+
+      // duration 후에 auto close 동작 실행
       setTimeout(() => {
         setToasts((prev) => prev.filter((toast) => toast.id !== id));
       }, duration);
