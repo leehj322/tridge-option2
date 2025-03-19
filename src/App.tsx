c22c7981f4f002b9868d3eb3dc84b21e03c0fe76
leehj322 <gudwns8956@naver.com>
2025-03-19 18:58:29 +0900

    feat: Add buttons for test toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import useToast from "./components/toast/useToast";
+
 function App() {
-  return <h2>My Toast</h2>;
+  const { showToastMessage } = useToast();
+
+  return (
+    <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <h2 className="mb-4 text-xl font-bold">My Toast</h2>
+      <div className="flex flex-col space-y-2">
+        <button
+          className="rounded-lg bg-blue-500 px-4 py-2 text-white shadow-md transition hover:bg-blue-600"
+          onClick={() => showToastMessage("Test toast message.")}
+        >
+          Open First Toast
+        </button>
+        <button
+          className="rounded-lg bg-green-500 px-4 py-2 text-white shadow-md transition hover:bg-green-600"
+          onClick={() =>
+            showToastMessage(
+              "Your 'Purchase Trade Contract' has been successfully canceled.",
+              "bottom-left",
+              null,
+            )
+          }
+        >
+          Open Second Toast
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
